8f1562a57071dafe0f883d393fc8fae000f54188
siddiq kolimi <siddiqkolimi.dev@gmail.com>
2023-11-24 10:55:13 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 
 //dynamic port
-const port = process.env.PORT || 1303;
+const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 app.use(router);
